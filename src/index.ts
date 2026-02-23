a4d5c7dae0a86553f5a08ac4957cbadc4de7eaa7
Pushpa123-code <rmanabendra733@gmail.com>
2026-02-23 09:54:09 +0530

    Made changes here

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,10 @@ app.use(express.json())
 app.get("/" , (req,res)=>{
   res.json({msg : "hey"})
 })
+app.get("/get" , (req,res)=>{
+  res.json({msg : "hey is this get endpoint?"})
+})
+app.get("/" , (req,res)=>{
+  res.json({msg : "hey"})
+})
 app.listen(port,() => {console.log(`connected to port ${port}`)} )
